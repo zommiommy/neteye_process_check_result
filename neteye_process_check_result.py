1d393d27211174aeb5c858a9b7d6a91c6cecc6f2
Tommaso Fontana <tommaso.fontana@mail.polimi.it>
2020-04-02 17:11:50 +0200

    added __name__

--- neteye_process_check_result.py
+++ neteye_process_check_result.py
@@ -140,32 +140,32 @@ def create_host():
 ####################################################################################################
 # Arguments parsing
 ####################################################################################################
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Execute Icinga2's process check result and, if needed, create hostname and/or service"
+    )
+    parser.add_argument("host", type=str, help="")
+    parser.add_argument("host_template", type=str, help="")
+    parser.add_argument("service", type=str, help="")
+    parser.add_argument("service_template", type=str, help="")
+    parser.add_argument("plugin_output", type=str, help="")
+    parser.add_argument("exit_status", type=int, help="")
+    parser.add_argument("rule", type=str, help="")
 
-parser = argparse.ArgumentParser(
-    description="Execute Icinga2's process check result and, if needed, create hostname and/or service"
-)
-parser.add_argument("host", type=str, help="")
-parser.add_argument("host_template", type=str, help="")
-parser.add_argument("service", type=str, help="")
-parser.add_argument("service_template", type=str, help="")
-parser.add_argument("plugin_output", type=str, help="")
-parser.add_argument("exit_status", type=int, help="")
-parser.add_argument("rule", type=str, help="")
+    args = vars(parser.parse_args())
 
-args = vars(parser.parse_args())
+    args = {
+        k: urllib.urlencode(v)
+        for k, v in args.items()
+    }
 
-args = {
-    k: urllib.urlencode(v)
-    for k, v in args.items()
-}
+    # Remove eventual spaces and sharps from the service name
+    args["service"] = re.sub(r"[# ]+", r"_", args["service"])
 
-# Remove eventual spaces and sharps from the service name
-args["service"] = re.sub(r"[# ]+", r"_", args["service"])
+    # Setup the logger
+    logging.basicConfig(
+        filename=LOG_FILE.format(**args),
+        level=logging.INFO
+    )
 
-# Setup the logger
-logging.basicConfig(
-    filename=LOG_FILE.format(**args),
-    level=logging.INFO
-)
-
-process_check_result()
+    process_check_result()
