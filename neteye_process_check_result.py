64021ac065dcfec6a04fd41c9dc26c0a10145c0d
t <t>
2020-05-19 11:58:19 +0200

    added locking to creations

--- neteye_process_check_result.py
+++ neteye_process_check_result.py
@@ -3,18 +3,20 @@ import os
 import re
 import sys
 import uuid
-import time
+import fcntl
 import urllib
 import logging
 import requests
 import argparse
+from time import sleep
+
 ####################################################################################################
 # Constants
 ####################################################################################################
-NETEYE_URL = """https://100.67.0.25:5665"""
+NETEYE_URL = """https://monitor.irideos.it:5665"""
 USER="director"
 
-PW_FILE="/neteye/shared/icinga2/conf/icinga2/conf.d/director-user.conf"
+PW_FILE="/neteye/shared/tornado/data/director-user.conf"
 with open(PW_FILE) as f:
     match = re.search(r"password *= *\"(.+)\"", f.read())
     if match:
@@ -23,11 +25,22 @@ with open(PW_FILE) as f:
         print("CANNOT FIND PASSWORD")
         sys.exit(-1)
 
+####################################################################################################
+# Decorators
+####################################################################################################
 
-LOG_FILE = """/neteye/shared/tornado/data/archive/neteye_process_check_result.log"""
-####################################################################################################
-# Functions
-####################################################################################################
+def lock(path):
+    """I have already checked, the unlock works even if the subfunction exits with sys.exit"""
+    def lock_internal(function):
+        def wrapped(*args, **kwargs):
+            with open(path, "a") as f:
+                fcntl.flock(f.fileno(), fcntl.LOCK_EX)
+                result = function(*args, **kwargs)
+                fcntl.flock(f.fileno(), fcntl.LOCK_UN) 
+                return result
+        return wrapped
+    return lock_internal
+
 def retry(max_times=30, sleep_time=3):
     def retry_decorator(function):
         def wrapped(*args, **kwargs):
@@ -38,84 +51,17 @@ def retry(max_times=30, sleep_time=3):
                         return result
                 except requests.exceptions.Timeout:
                     logging.warning("The function %s went in timeout", function.__name__)
-                time.sleep(sleep_time)
+                sleep(sleep_time)
             logging.warning(" [EXIT] %s reached max number of tries from the arguments %s", function.__name__, args)
             sys.exit(2)
         return wrapped
     return retry_decorator
 
+####################################################################################################
+# Requests
+####################################################################################################
 
-@retry()
-def process_check_result():
-    url = NETEYE_URL +  "/v1/actions/process-check-result"
-    logging.info("[PC] process_check_result on url %s", url)
-
-    data = {
-            "type": "Service",
-            "filter": "host.name==\"{host}\" && service.name==\"{service}\"".format(**args),
-            "exit_status":args["exit_status"],
-            "plugin_output":args["plugin_output"],
-            "check_source":os.uname()[1],
-        }
-
-    r = requests.post(
-        url,
-        json=data,
-        auth=(USER, PW), verify=False,
-        headers={
-            'Accept': 'application/json',
-        }
-    )
-
-
-    if r.status_code == 200:
-        data = r.json()
-        if data["results"] == []:
-            logging.info("[PC] KO : %s", data)
-            create_service()
-        else:
-            logging.info("[PC] OK : %s", data)
-            return data
-
-    logging.warning("[PC] Error : %s", r.text.replace("\n", ""))
-    if r.status_code in [500, 503]:
-        create_service()
-
-
-@retry()
-def create_service():
-    url = "{neteye_url}/v1/objects/services/{host}!{service}".format(
-        neteye_url=NETEYE_URL,
-        host=urllib.quote(args["host"]).replace("/", "%2F"),
-        service=urllib.quote(args["service"]).replace("/", "%2F"),
-    )
-    logging.info("[SC] creating service on url %s", url)
-
-    data = {
-            "templates":[args["service_template"]],
-            "attrs":{
-            }
-    }
-
-    r = requests.put(
-        url,
-        json=data,
-        auth=(USER, PW), verify=False,
-        headers={
-            'Accept': 'application/json',
-        }
-    )
-
-    if r.status_code == 200:
-        logging.info("[SC] OK : %s", r.json())
-        return r.json()
-    
-    logging.warning("[SC] Error : %s", r.text.replace("\n", ""))
-    if r.status_code in [500, 503]:
-        create_host()
-
-@retry()
-def create_host():
+def create_host_request(args):
     url = "{neteye_url}/v1/objects/hosts/{host}".format(
         neteye_url=NETEYE_URL,
         host=urllib.quote(args["host"]).replace("/", "%2F")
@@ -139,12 +85,111 @@ def create_host():
         }
     )
 
-    if r.status_code == 200:
-        logging.info("[HC] OK : %s", r.json())
-        return r.json()
-    elif r.status_code in [500, 503]:
-        logging.warning("[HC] got error: %s", r.json())
-        sys.exit(2)
+    try:
+        data = r.json()
+    except:
+        data = r.text
+
+    return r.status_code, data, r.text
+
+def create_service_request(args):
+    url = "{neteye_url}/v1/objects/services/{host}!{service}".format(
+        neteye_url=NETEYE_URL,
+        host=urllib.quote(args["host"]).replace("/", "%2F"),
+        service=urllib.quote(args["service"]).replace("/", "%2F"),
+    )
+    logging.info("[SC] creating service on url %s", url)
+
+    data = {
+            "templates":[args["service_template"]],
+            "attrs":{
+            }
+    }
+
+    r = requests.put(
+        url,
+        json=data,
+        auth=(USER, PW), verify=False,
+        headers={
+            'Accept': 'application/json',
+        }
+    )
+
+    try:
+        data = r.json()
+    except:
+        data = r.text
+
+    return r.status_code, data, r.text
+
+def process_check_result_request(args):
+    url = NETEYE_URL +  "/v1/actions/process-check-result"
+    logging.info("[PC] process_check_result on url %s", url)
+
+    data = {
+            "type": "Service",
+            "filter": "host.name==\"{host}\" && service.name==\"{service}\"".format(**args),
+            "exit_status":args["exit_status"],
+            "plugin_output":args["plugin_output"],
+            "check_source":os.uname()[1],
+        }
+
+    r = requests.post(
+        url,
+        json=data,
+        auth=(USER, PW), verify=False,
+        headers={
+            'Accept': 'application/json',
+        }
+    )
+    
+    try:
+        data = r.json()
+    except:
+        data = r.text
+
+    return r.status_code, data, r.text
+
+
+####################################################################################################
+# Functions
+####################################################################################################
+
+@lock("/var/lock/process_check_result.create_host.lock")
+@retry()
+def create_host(args):
+    status_code, data, text = create_host_request(args)
+
+    if status_code == 200:
+        logging.info("[HC] OK : %s", data)
+        return data
+
+    logging.warning("[HC] got error: %s", text)
+    sys.exit(2)
+
+
+@lock("/var/lock/process_check_result.create_service.lock")
+@retry()
+def create_service(args):
+    status_code, data, text = create_service_request(args)
+
+    if status_code == 200:
+        logging.info("[SC] OK : %s", data)
+        return data
+    
+    logging.warning("[SC] Error : %s", text.replace("\n", ""))
+    create_host(args)
+
+@retry()
+def process_check_result(args):
+    status_code, data, text = process_check_result_request(args)
+    if status_code == 200 and data["results"] != []:
+        logging.info("[PC] OK : %s", data)
+        return data
+
+    logging.warning("[PC] Error : %s", text.replace("\n", ""))
+    create_service(args)
+    
 
 ####################################################################################################
 # Arguments parsing
@@ -159,6 +204,7 @@ if __name__ == "__main__":
     parser.add_argument("service_template", type=str, help="")
     parser.add_argument("plugin_output", type=str, help="")
     parser.add_argument("exit_status", type=int, help="")
+    parser.add_argument("log-file", type=str, help="")
 
     args = vars(parser.parse_args())
 
@@ -172,7 +218,7 @@ if __name__ == "__main__":
     shandler.setFormatter(formatter)
     logger.addHandler(shandler)
 
-    fhandler = logging.FileHandler(LOG_FILE.format(**args))
+    fhandler = logging.FileHandler(args["log_file"])
     fhandler.setLevel(logging.INFO)
     fhandler.setFormatter(formatter)
     logger.addHandler(fhandler)
@@ -181,6 +227,6 @@ if __name__ == "__main__":
     # requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
 
     logging.info("START")
-    data = process_check_result()
+    data = process_check_result(args)
     logging.info("[PC] OK : %s", data["results"])
     logging.info("STOP")
